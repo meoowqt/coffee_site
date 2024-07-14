d27bd9a2c0b7ccf0ed7b501c5f7ea2d10e2b2c37
Vechkanova <vechkanova.p@gmail.com>
2024-07-14 15:59:41 +0400

    Добавлена база данных для комментариев

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from flask import Flask, render_template, request, flash, redirect
+from sqlalchemy import desc
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql://postgres:1234@localhost:5432/postgres"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+
+
+class Comment(db.Model):
+    __tablename__ = "comment"
+
+    comment_id = db.Column(
+        db.Integer(), primary_key=True, nullable=False, autoincrement=True
+    )
+    comment_text = db.Column(db.Text(), nullable=False)
+    commentator_name = db.Column(db.Text(), nullable=False)
+
+    def __init__(self, _name, _text):
+        self.commentator_name = _name
+        self.comment_text = _text
+
+with app.app_context():
+    db.create_all()
+    
 @app.route("/")
 def index():
     context = {
@@ -11,11 +37,18 @@ def index():
     return render_template("coffee_site/index.html", context=context)
 
 
-@app.route("/comments")
+@app.route("/comments", methods=['POST', 'GET'])
 def comments():
     context = {
         "title": "Отзывы",
     }
+    if request.method == ['POST']:
+        name = request.form['name']
+        text = request.form['text']
+        comm = Comment(_text=text, _name=name)
+        db.session.add(comm)
+        db.session.commit()
+    comms = Comment.query.order_by(desc(Comment.comment_id)).all()
     return render_template("coffee_site/comments.html", context=context)
 
 
